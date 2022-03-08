6d428ec461146e069e6245d77cfce16fdae1f5ec
humam.husam.eldeen@gmail.com <humam.husam.eldeen@gmail.com>
2022-03-08 13:59:23 +0200

    Add PORT

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.PORT);
-  await app.listen(parseInt(process.env.PORT) | 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
